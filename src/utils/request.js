72baf88e284d9123438aa3351b900b6e0931057d
111inhistory <59101115+111inhistory@users.noreply.github.com>
2024-11-03 10:53:36 +0800

    test force using eapi

--- src/utils/request.js
+++ src/utils/request.js
@@ -40,6 +40,8 @@ service.interceptors.request.use(function (config) {
 
   if (!config.params.ua) config.params.ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36 Chrome/91.0.4472.164 NeteaseMusicDesktop/3.0.18.203152';
 
+  if (!config.params.crypto) config.params.crypto = 'eapi';
+
   // Force real_ip
   const enableRealIP = JSON.parse(
     localStorage.getItem('settings')
